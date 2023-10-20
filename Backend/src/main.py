cb66d96787ae8d7d5e076030381f7c3bf67955ab
Tekai_ <vip007_007@mail.ru>
2023-10-20 19:28:27 +0300

    it's work!

--- Backend/src/main.py
+++ Backend/src/main.py
@@ -1,9 +1,28 @@
-from fastapi import FastAPI
+import time
+from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from src.todo.router import router as router_todo
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="ToDo App")
+origins = [
+    "http://localhost:5173",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT"],
+    allow_headers=["Content-Type", "Set-Cookie", "Access-Control-Allow-Headers", "Access-Control-Allow-Origin",
+                   "Authorization"],
+)
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+    return response
 app.include_router(router_todo)
 
 app.mount("/static", StaticFiles(directory="src/static"), name="static")
--- Frontend/src/pages/test/index.tsx
+++ Frontend/src/pages/test/index.tsx
@@ -1,5 +1,15 @@
+// import axios from "axios";
+// import { IData } from "./index.interface";
+
 const TestPage = () => {
-  return <div>Test Page</div>;
+  // const func = () => {
+  //   const data = axios
+  //     .get<IData, string>("http://127.0.0.1:8000/vova")
+  //     .then((res) => JSON.parse(res));
+  //   return data;
+  // };
+
+  return <div>Hi</div>;
 };
 
 export default TestPage;
