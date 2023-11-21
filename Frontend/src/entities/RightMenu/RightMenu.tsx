a8fb406a7c3f2a947d376e65e30e545f56cfe89d
Tekai_ <vip007_007@mail.ru>
2023-11-21 13:46:41 +0300

    Fix

--- Frontend/src/entities/RightMenu/RightMenu.tsx
+++ Frontend/src/entities/RightMenu/RightMenu.tsx
@@ -12,7 +12,7 @@ import { selectCurrentTask } from "widgets/todosList/model/PanelsSelectors";
 const menuAnimation = {
   initial: { opacity: 0, x: 100 },
   animate: { opacity: 1, x: 0 },
-  exit: { opacity: 0, x: 300 },
+  exit: { opacity: 0, x: 400 },
 };
 
 const RightMenu = () => {
@@ -38,13 +38,13 @@ const RightMenu = () => {
           })}
         >
           <Close onClick={handleClick} />
-          <div className="flex items-center justify-between flex-col gap-4 h-full">
+          <div className="flex items-center justify-between flex-col gap-4 h-full p-5">
             <form
-              className="flex items-center flex-col gap-4 p-5 h-fit"
+              className="flex items-start flex-col gap-4 p-5 h-fit border mt-10 w-full rounded ml-5"
               onSubmit={handleSubmit}
             >
-              <div>{menuData?.title}</div>
-              <div>{menuData?.description}</div>
+              <div className="text-2xl font-medium">{menuData?.title}</div>
+              <div className="text-xl">{menuData?.description}</div>
               <Btn>Save</Btn>
             </form>
             <div>Added</div>
--- Frontend/src/shared/ui/close/Close.tsx
+++ Frontend/src/shared/ui/close/Close.tsx
@@ -2,7 +2,13 @@ import { CloseButton, Stack } from "@chakra-ui/react";
 
 const Close = ({ onClick }: { onClick: () => void }) => {
   return (
-    <Stack direction="row" spacing={6} onClick={onClick} color={"GrayText"}>
+    <Stack
+      direction="row"
+      spacing={6}
+      onClick={onClick}
+      color={"GrayText"}
+      className="absolute top-0 right-0"
+    >
       <CloseButton size="lg" />
     </Stack>
   );
