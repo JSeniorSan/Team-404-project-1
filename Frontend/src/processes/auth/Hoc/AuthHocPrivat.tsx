3edfbf0fb09f5980f27ad9d0bc97dbb2b09267fa
Tekai_ <vip007_007@mail.ru>
2024-01-23 17:12:21 +0300

    auth hoc fix

--- Frontend/src/processes/auth/Hoc/AuthHocPrivat.tsx
+++ Frontend/src/processes/auth/Hoc/AuthHocPrivat.tsx
@@ -1,16 +1,22 @@
 import { Navigate, useLocation } from "react-router-dom";
-import React from "react";
+import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { selectUser } from "shared/api/user/userSelectors";
+import { todoApi } from "shared/api/todoQueryApi/TodoServise";
 
 export interface IAuthHoc {
   children: React.ReactNode;
 }
 
 const AuthHocPrivat: React.FC<IAuthHoc> = ({ children }) => {
+  const [getMeQuery, { data: meData }] = todoApi.useLazyGetMeQuery();
+  useEffect(() => {
+    getMeQuery(null);
+  }, [getMeQuery]);
   const selectUserStore = useSelector(selectUser);
   const location = useLocation();
-  if (Object.keys(selectUserStore).length === 0) {
+
+  if (Object.keys(selectUserStore).length === 0 && !meData) {
     return <Navigate to="/account" state={{ from: location }} replace />;
   }
   return children;
